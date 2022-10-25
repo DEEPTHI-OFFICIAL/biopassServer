d701d90a6f97a67f3b2cb113b0de52c114abf190
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-10-25 19:57:26 +0530

    putting in heroku

--- bioserver.js
+++ bioserver.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -9,4 +10,4 @@ const userRouter = require("./src/routes/bioRoutes");
 
 app.use("/biopass", userRouter);
 
-app.listen(3000);
+app.listen(port);
