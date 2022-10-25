3a5967c3b59264c4c3a2611f03ef0cdbb76ff4e9
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-10-25 07:28:17 +0530

    commit

--- bioserver.js
+++ bioserver.js
@@ -3,7 +3,7 @@ const WebsiteList = require("./schema/bioSchema");
 const app = express();
 const mongoose = require("mongoose");
 
-//"mongodb://mongodb+srv://appu:<#Appu2705>@cluster0.v4umeji.mongodb.net/?retryWrites=true&w=majority"
+//"mongodb://mongodb+srv://appu:<password>@cluster0.v4umeji.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose
   .connect("mongodb://0.0.0.0:27017/biopass", {
