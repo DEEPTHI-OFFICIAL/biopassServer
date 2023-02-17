605fafc9d90c2e1514268fc725979608f078c7e4
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2023-02-17 20:03:40 +0530

    new db

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-//atlas = "mongodb+srv://appu:appu@cluster0.v4umeji.mongodb.net/?retryWrites=true&w=majority"
+atlas =
+  "mongodb+srv://appu:appu@cluster0.obhakyn.mongodb.net/?retryWrites=true&w=majority";
 //local = "mongodb://0.0.0.0:27017/biopass"
 // const db = "mongodb+srv://appu:appu@cluster0.jme0etq.mongodb.net/test";
 
 mongoose.Promise = global.Promise;
 
 mongoose
-  .connect(process.env.DB, {
+  .connect(atlas, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
