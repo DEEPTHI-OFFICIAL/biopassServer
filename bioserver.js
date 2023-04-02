d518ae373063fc0ca5e441f96ecc2b54e5b01597
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2023-04-02 22:48:49 +0530

    sockets done

--- bioserver.js
+++ bioserver.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const socket = require("socket.io");
+const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
@@ -12,4 +14,15 @@ const userRouter = require("./src/routes/bioRoutes");
 
 app.use("/biopass", userRouter);
 
-app.listen(port);
+const server = app.listen(port);
+
+const io = socket(server);
+//Socket.io Connection------------------
+io.on("connection", (socket) => {
+  console.log("New socket connection: " + socket.id);
+
+  socket.on("checkSocket", () => {
+    console.log("worked socket");
+    io.emit("checkSocket", "worked Sockets");
+  });
+});
