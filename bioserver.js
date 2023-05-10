fbba9323883541c6e3fbae971f53888401696852
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2023-05-10 23:20:30 +0530

    no cors

--- bioserver.js
+++ bioserver.js
@@ -16,10 +16,6 @@ const userRouter = require("./src/routes/bioRoutes");
 app.use("/biopass", userRouter);
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
   next();
 });
 
